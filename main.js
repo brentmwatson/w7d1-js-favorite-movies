8d41a9bf26e6ad211cd191d8308e9ce8317ccd2d
Brent Watson <brentmwatson@gmail.com>
2016-11-10 00:57:20 -0500

    list added

--- main.js
+++ main.js
@@ -4,27 +4,27 @@ movies=[]
 // Dynamically add 5 of your favorite movies to the array, one by one (rather than at creation time)
 movies.push({title: "Spaceballs",
             director: "Mel Brooks",
-            actors: ["Mel Brooks, John Candy, Rick Moranis"],
+            actor_list: ["Mel Brooks, John Candy, Rick Moranis"],
             genre: "Comedy",
             year: "1987" })
 movies.push({title: "The Dark Knight",
             director: "Christopher Nolan",
-            actors: ["Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine, Maggie Gyllenhaal"],
+            actor_list: ["Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine, Maggie Gyllenhaal"],
             genre: "Action",
             year: "2008" })
 movies.push({title: "American Hustle",
             director: "David O. Russell",
-            actors: ["Christian Bale, Bradley Cooper, Amy Adams, Jeremy Renner, Jennifer Lawrence"],
+            actor_list: ["Christian Bale, Bradley Cooper, Amy Adams, Jeremy Renner, Jennifer Lawrence"],
             genre: "Suspence",
             year: "2013" })
 movies.push({title: "Donnie Darko",
             director: "Richard Kelly",
-            actors: [" Jake Gyllenhaal, Jena Malone, Mary McDonnel"],
+            actor_list: [" Jake Gyllenhaal, Jena Malone, Mary McDonnel"],
             genre: "Thriller",
             year: "2001"})
 movies.push({title: "The Machinist",
             director: "Brad Anderson",
-            actors: [" Christian Bale, Jennifer Jason Leigh, Aitana Sánchez-Gijón"],
+            actor_list: [" Christian Bale, Jennifer Jason Leigh, Aitana Sánchez-Gijón"],
             genre: "Thriller",
             year: "2004"})
 // Make sure each movie object has a title, director list, genre, year, and actor list
